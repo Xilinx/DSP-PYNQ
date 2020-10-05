94498124886c86615a55eb539e392c7c7aad5cd3
Sarunas Kalade <sarunas.kalade@strath.ac.uk>
2020-10-05 21:14:17 +0300

    DSP-PYNQ upgrade to 2020.1, scipy upgrade for deprecation warnings (#5)
    * upgraded ZCU111 design to Vivado 2020.1
    
    * updated block design to Vivado 2020.1
    
    * xlnk deprecated, replaced with allocate
    
    * added zcu111 bitstream
    
    * added scipy 1.12 dependency
    
    * Ultra96 upgrade to 2020.1
    
    Co-authored-by: Lewis Davin McLaughlin <47351729+LewisMcL@users.noreply.github.com>

--- boards/Ultra96/create_project.tcl
+++ boards/Ultra96/create_project.tcl
@@ -1,7 +1,7 @@
 set proj_name "dsp_pynq"
 
-create_project ${proj_name} ./${proj_name} -part xczu3eg-sbva484-1-e
-set_property BOARD_PART em.avnet.com:ultra96v2:part0:1.0 [current_project]
+create_project ${proj_name} ./${proj_name} -part xczu3eg-sbva484-1-i
+#set_property BOARD_PART em.avnet.com:ultra96v2:part0:1.0 [current_project]
 set_property target_language VHDL [current_project]
 
 source ./block_design.tcl
@@ -11,7 +11,7 @@ update_compile_order -fileset sources_1
 launch_runs impl_1 -to_step write_bitstream -jobs 16
 wait_on_run impl_1
 
-# get bitstream and hwh files
+#get bitstream and hwh files
 if {![file exists ./bitstreams/]} {
 	file mkdir ./bitstreams/
 	}
